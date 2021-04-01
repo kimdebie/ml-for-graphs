1f06c90179c9fd53d4276a05547456002f0e0ca3
Ruud Schramp (DBS) <schramp@holmes.nl>
2021-04-01 10:29:18 +0200

    Generator, just comment

---
+++ generator/generate_cdr.py
@@ -0,0 +1,12 @@
+"""Generate CDR's for a virtual user scenario:
+
+N persons form a criminal collaborating group.
+They have a constant physical relation of M friends with which they make calls.
+
+Missing in the model is a hierarchy
+
+To ensure their privacy the persons hold P phones with a certain number.
+
+During a certain amount of time they will make calls.
+
+"""
